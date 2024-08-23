36cca142e61f8e901a9364a6e4fa1e7c2573c436
THE-G0OSE <nikitos54345@gmail.com>
2024-08-23 19:23:48 +0300

    Summary
    Basic player and camera movment

---
+++ Assets/scripts/cameraFollowing.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class cameraFollowing : MonoBehaviour
+{
+    [Header("»грок")]
+    [SerializeField] private player target;
+    [Header("Ќастройки камеры")]
+    [Tooltip("врем€ за которое камера нагон€ет цель")]
+    [SerializeField] private float followingTime;
+    [Tooltip("смещение по горизонтали в сторону движени€ игрока")]
+    [SerializeField] private float horizontalOrientationDelta;
+
+    private Vector3 cameraPosition;
+    private Vector3 targetPosition;
+    private Vector3 distanceVector;
+    private float currentHorizontalOrientationDelta;
+    //private player.playerHorizontalOrientation playerHorizontalOrientation;
+    private void Update()
+    {
+//positions and player orientation detection
+
+        //playerHorizontalOrientation = target.currentPlayerHorizontalOrientation;
+        //float currentHorizontalOrientationDelta = (playerHorizontalOrientation == player.playerHorizontalOrientation.Right) ? horizontalOrientationDelta : -horizontalOrientationDelta; 
+        cameraPosition = transform.position;
+        targetPosition = new Vector3(target.transform.position.x + currentHorizontalOrientationDelta, target.transform.position.y, target.transform.position.z);
+
+//distance form camera to target
+
+        distanceVector = new Vector3(-(cameraPosition.x - targetPosition.x), -(cameraPosition.y - targetPosition.y), 0);
+
+//smooth camera moving
+
+        transform.Translate(distanceVector.x / followingTime, distanceVector.y / followingTime, distanceVector.z / followingTime);
+        
+        
+    }
+    public void SetHorizontalOrientation(player.playerHorizontalOrientation orientation)
+    {
+        switch (orientation)
+        {
+            case player.playerHorizontalOrientation.Left: currentHorizontalOrientationDelta = -horizontalOrientationDelta; break;
+            case player.playerHorizontalOrientation.Right: currentHorizontalOrientationDelta = horizontalOrientationDelta; break;
+        }
+    }
+
+
+
+}
---
+++ Assets/scripts/player.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Aseprite;
+using UnityEngine;
+
+public class player : MonoBehaviour
+
+{
+    [SerializeField] private cameraFollowing camera;
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float secondJumpForce;
+    [SerializeField] private float orientationChangeDelay;
+
+    public enum playerHorizontalOrientation { Left = 1, Right = 2 }
+    public playerHorizontalOrientation currentPlayerHorizontalOrientation = playerHorizontalOrientation.Right;
+    private int maxJumps = 1;
+    private int amountOfJumps;
+    private bool OnGround;
+    private Rigidbody2D rb;
+    private bool isRunning;
+    private float sprint;
+    private playerHorizontalOrientation lastOrientation = playerHorizontalOrientation.Right;
+
+    private void Start()
+    {
+        StartCoroutine(ChangeOrientation());
+    }
+    IEnumerator ChangeOrientation()
+    {
+        float stableOrientationTime = 0;
+        while (true)
+        {
+            if (currentPlayerHorizontalOrientation == lastOrientation)
+            {
+                stableOrientationTime += 0.1f;
+                if (stableOrientationTime > orientationChangeDelay)
+                {
+                    camera.SetHorizontalOrientation(currentPlayerHorizontalOrientation);
+                }
+            }
+            else
+            {
+                stableOrientationTime = 0;
+            }
+            lastOrientation = currentPlayerHorizontalOrientation;
+            yield return new WaitForSeconds(0.1f);
+
+        }
+
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+
+
+//horizontal moving and orientation
+        
+        
+        if (Input.GetKey(KeyCode.LeftShift)) { sprint = 1.5f; } else { sprint = 1f; }
+
+
+        if (Input.GetKey("d") || Input.GetKey("a"))
+        {
+            float xSpeed = (Input.GetKey("d")) ? playerSpeed * sprint : -playerSpeed * sprint ;
+            currentPlayerHorizontalOrientation = (Input.GetKey("d")) ? playerHorizontalOrientation.Right : playerHorizontalOrientation.Left;
+            this.transform.position = new Vector3(this.transform.position.x + xSpeed*Time.deltaTime, this.transform.position.y, this.transform.position.z);
+        }
+        
+        
+//Jump mechanic
+
+        
+        if (Input.GetKeyDown("space") && OnGround == true)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown("space") && OnGround == false && amountOfJumps > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(new Vector3(0, secondJumpForce, 0), ForceMode2D.Impulse);
+            amountOfJumps -= 1;
+        }
+        
+    }
+
+//Grounded status
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            OnGround = true;
+            amountOfJumps = maxJumps;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            OnGround = false;
+        }
+    }
+}
+
